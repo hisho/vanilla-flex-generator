fa4237b4ef8e1531b6de9031bb77f385092a108e
HIsho Ohmasu <hisyou000@gmail.com>
2019-11-04 20:45:55 +0900

    🎉 Development environment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # grid-generator
-grid-generator
+# [https://hishohub.github.io/grid-generator/](https://hishohub.github.io/grid-generator/)
+
+## コマンド
+****
+
+### インストール
+```node
+$ npm ci
+```
+### 実行
+```node
+$ npm run watch
+```
---
+++ webpack.mix.js
@@ -0,0 +1,57 @@
+const
+mix = require('laravel-mix'), //laravel-mix本体
+StyleLintPlugin = require('stylelint-webpack-plugin'); // stylelintを使う
+
+
+const dist = `docs/assets/`;
+/*
+  |--------------------------------------------------------------------------
+  | Mix Asset Management
+  |--------------------------------------------------------------------------
+  |
+  | Mix provides a clean, fluent API for defining some Webpack build steps
+  | for your Laravel application. By default, we are compiling the Sass
+  | file for the application as well as bundling up all the JS files.
+  |
+ */
+
+mix.sass(`src/scss/style.scss`, `${dist}css`)
+  .options({
+    processCssUrls: false,
+    postCss: [require("autoprefixer")({
+      grid: 'autoplace'
+    }), require("css-mqpacker"), ],
+  })
+  .sourceMaps()
+
+mix.js(`src/js/main.js`, `${dist}js`).sourceMaps();
+
+mix.webpackConfig({
+  module: {
+    rules: [{
+      test: /\.scss/,
+      loader: 'import-glob-loader'
+    }]
+  },
+  plugins: [
+    new StyleLintPlugin({
+      fix: true
+    })
+  ]
+})
+
+
+mix.webpackConfig({
+  devtool: "inline-source-map"
+});
+
+
+mix.browserSync({
+  server: 'docs',
+  proxy: false,
+  files: [
+    'docs/index.html',
+    'docs/assets/js/*.js',
+    'docs/assets/css/*.css',
+  ]
+})
